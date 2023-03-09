6f99eb9c8d50647f5ec394622580d85ec71f10e3
Gg123 <li.c@mail.ru>
2023-03-09 23:16:25 +0300

    генерация данных с Faker

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,7 +1,11 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
+
+import static tests.TestData.*;
+import static utils.RandomUtils.getRandomItemFromArray;
 
 public class RegistrationWithPageObjectsTests {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -9,27 +13,28 @@ public class RegistrationWithPageObjectsTests {
 
     @Test
     public void toolsQaForm () {
-        String userName = "Galina";
-        String lastName = "Ponomareva";
-        String userEmail = "li.c@mail.ru";
-        String userGenter = "Female";
-        String userNumber = "9096201240";
-        String userBirthDay_day = "30";
-        String userBirthDay_month = "January";
-        String userBirthDay_year = "1990";
-        String userSubjects = "Physics";
-        String userHobbies = "Reading";
+        Faker faker = new Faker();
+        String userName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String userEmail = faker.internet().emailAddress();
+        String userGender = getRandomItemFromArray(gender);
+        String userNumber = 8 + faker.phoneNumber().subscriberNumber(9);
+        String userBirthDay_day = String.format("%02d", faker.number().numberBetween(1, 28));
+        String userBirthDay_month = getRandomItemFromArray(months);
+        String userBirthDay_year = String.valueOf(faker.number().numberBetween(1950, 2005));
+        String userSubjects = getRandomItemFromArray(subjects);
+        String userHobbies = getRandomItemFromArray(hobbies);
         String userPictureLocation = "pictures/3.png";
-        String userAddress = "Lesoparkovaya str 4";
+        String userAddress = faker.address().fullAddress();
         String userState = "NCR";
-        String userCity = "Delhi";
+        String userCity = getRandomItemFromArray(cities);
 
         registrationPage.openPage();
 
         registrationPage.setFirstName(userName);
         registrationPage.setLastName(lastName);
         registrationPage.setEmail(userEmail);
-        registrationPage.setGenter(userGenter);
+        registrationPage.setGender(userGender);
         registrationPage.setNumber(userNumber);
         registrationPage.setBirthDay(userBirthDay_day, userBirthDay_month, userBirthDay_year);
         registrationPage.setSubjects(userSubjects);
@@ -40,19 +45,18 @@ public class RegistrationWithPageObjectsTests {
         registrationPage.setCity(userCity);
 
         registrationPage.clickSubmit();
-        registrationPage.verifyResoultsModal();
-        registrationPage.verifyResoults("Student Name", userName + " " + lastName);
-        registrationPage.verifyResoults("Student Email", userEmail);
-        registrationPage.verifyResoults("Gender", userGenter);
-        registrationPage.verifyResoults("Mobile", userNumber);
-        registrationPage.verifyResoults("Date of Birth", userBirthDay_day + " " + userBirthDay_month + "," + userBirthDay_year);
-        registrationPage.verifyResoults("Subjects", userSubjects);
-        registrationPage.verifyResoults("Hobbies", userHobbies);
-        registrationPage.verifyResoults("Address", userAddress);
-        registrationPage.verifyResoults("State and City", userState + " " + userCity);
+        registrationPage.verifyResultsModal();
+        registrationPage.verifyResults("Student Name", userName + " " + lastName);
+        registrationPage.verifyResults("Student Email", userEmail);
+        registrationPage.verifyResults("Gender", userGender);
+        registrationPage.verifyResults("Mobile", userNumber);
+        registrationPage.verifyResults("Date of Birth", userBirthDay_day + " " + userBirthDay_month + "," + userBirthDay_year);
+        registrationPage.verifyResults("Subjects", userSubjects);
+        registrationPage.verifyResults("Hobbies", userHobbies);
+        registrationPage.verifyResults("Address", userAddress);
+        registrationPage.verifyResults("State and City", userState + " " + userCity);
 
         registrationPage.clickClose();
 
     }
 }
-
